e735808a862071ce00edeae6c375b7b43854e7b2
Nicholas Oliveira dos Santos <nicolasgoldtec@gmail.com>
2021-02-26 00:29:53 +0000

    Verify return age

--- src/main/java/br/com/impacta/lab/controller/CondicionalController.java
+++ src/main/java/br/com/impacta/lab/controller/CondicionalController.java
@@ -24,9 +24,13 @@ public class CondicionalController {
 		 *  Para menores de dezoito anos -> Possui menos de 18 anos
 		 *  
 		 */
+		if(idade>=18){
+		  return ResponseEntity.ok("Possui mais de 18 anos");
+		}else{
+		  return ResponseEntity.ok("Possui menos de 18 anos");
+		}
 		
 		
-		return ResponseEntity.ok("APÓS O ALGORITMO COLOCAR O TEXTO/VARIAVEL DE RETORNO AQUI");
 	}
 	
 }
